1da3afc377b26a8a7ead052c10c185e0f5a3cf42
Daniel Hemmen <info@danielhemmen.dev>
2025-11-30 21:58:07 +0100

    fix headless configuration

---
+++ frontend/karma.conf.js
@@ -0,0 +1,36 @@
+// Minimal Karma configuration for CI/CD
+// Uses Puppeteer's bundled Chromium which works in headless environments
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/cv-viewer'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    }
+  });
+};
